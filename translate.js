8c8a2517bb6ce945834c06c89d91bfb399f2630d
jaken92 <jaaakeeen@gmail.com>
2022-11-27 17:49:28 +0100

    external example for translation

--- translate.js
+++ translate.js
@@ -1,1 +1,11 @@
-s;
+const se = document.querySelector('.se');
+const en = document.querySelector('.en');
+
+const langButton = document.querySelector('.lang-btn');
+
+langButton.addEventListener('click', langFunc);
+
+function langFunc() {
+  se.classList.toggle('hide');
+  en.classList.toggle('hide');
+}
