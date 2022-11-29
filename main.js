ce327bc86634a1b70986ddd3af876e726835cf44
jaken92 <jaaakeeen@gmail.com>
2022-11-29 13:26:16 +0100

    translation work in progress
    Översättning tom naturligt material

---
+++ main.js
@@ -0,0 +1,13 @@
+/* translation */ 
+
+const swedish = document.querySelectorAll('.se');
+const english = document.querySelectorAll('.en');
+
+const langButton = document.querySelector('.lang-btn');
+
+langButton.addEventListener('click', langFunc);
+
+function langFunc() {
+  swedish.forEach((se) => se.classList.toggle('hide'));
+  english.forEach((en) => en.classList.toggle('hide'));
+}
